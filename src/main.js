99e51dda269e3cc765779c463f6592b559e360ad
Patryk Bernasiewicz <kontakt@patrykb.pl>
2019-07-14 02:13:38 +0200

    fixed problems with global styles importing

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import router from './router';
 import App from '@components/App';
-import styles from '@scss/reset.scss';
+import styles from '@scss/global.scss';
 
 new Vue({
-    router,
-    render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app');
